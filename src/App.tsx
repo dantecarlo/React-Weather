83e31a98c61c0debdbe077b1941dbba770709dff
dantecarlo <dante.cnp@gmail.com>
2019-10-05 13:11:52 -0500

    ADD HEader and Form Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Header from './components/Header'
+import Form from './components/Form'
 
 const App: React.FC = () => {
-  return <Header title="React Wheater"></Header>
+  return (
+    <div className="App">
+      <Header title="React Wheater"></Header>
+      <div className="form-container">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Form></Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,7 +5,26 @@ function Form() {
 
   return (
     <form className="input-filed col s12">
-      <input type="text" name="city" id="city" onChange={handleChange} />
+      <div className="input-field col s12">
+        <input type="text" name="city" id="city" onChange={handleChange} />
+        <label htmlFor="city">City: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select name="city" id="" onChange={handleChange}>
+          <option value="">Select a country</option>
+          <option value="PE">Peru</option>
+          <option value="AR">Argentina</option>
+          <option value="US">United States</option>
+          <option value="CO">Colombia</option>
+          <option value="MX">Mexico</option>
+          <option value="ES">Spain</option>
+        </select>
+      </div>
+
+      <div className="input-field col s12">
+        <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Search Weather" />
+      </div>
     </form>
   )
 }
